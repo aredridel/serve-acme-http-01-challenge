6ff1cf83c19751fd869c8a00a5538228ce9f7ba8
Aria Stewart <aredridel@nbtsc.org>
2016-09-25 14:38:19 -0400

    Add support for socket activation

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 const express = require('express');
 const path = require('path');
+const socketactivation = require('systemd-socket');
 
 const app = express();
 
@@ -8,15 +9,11 @@ if (!process.argv[2]) {
     console.warn("Supply a directory to serve!");
     process.exit(1);
 }
-if (!process.argv[3]) {
-    console.warn("Supply some place to listen");
-    process.exit(1);
-}
 
 const static = express.static(process.argv[2]);
 
 app.get('/.well-known/acme-challenge/:challenge', static);
-const server = app.listen(process.argv[3], (err) => {
+const server = app.listen(process.argv[3] || socketactivation(), (err) => {
     if (err) {
         console.warn(err);
         process.exit(1);
